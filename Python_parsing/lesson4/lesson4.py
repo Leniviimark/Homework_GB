7633ed9966e175b5e58d179934f7657541bc9be5
Leniviimark <countryhunter@mail.ru>
2021-10-10 18:41:47 +0300

    lesson4

--- Python_parsing/lesson4/lesson4.py
+++ Python_parsing/lesson4/lesson4.py
@@ -1,6 +1,12 @@
 from pprint import pprint
-from lxml import html
+
 import requests
+from lxml import html
+from pymongo import MongoClient
+
+client = MongoClient('127.0.0.1', 27017)
+db = client['test_database']
+news_db = db.news
 
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
@@ -8,6 +14,7 @@ url = "https://lenta.ru"
 response = requests.get(url, headers=header)
 dom = html.fromstring(response.text)
 
+# находим элементы через поиск части кода с часами, как было на разборе 4 урока
 items = dom.xpath("//time[contains(@class,'g-time')]/..")
 
 all_news = []
@@ -15,28 +22,18 @@ all_news = []
 for item in items:
     news = {}
     link = item.xpath("./@href")[0]
+    if link.find('https') != 0:
+        link = url + link
     title = item.xpath('./text()')[0].replace('\xa0', ' ')
     date = item.xpath('./time/@datetime')
     news['source'] = 'lenta.ru'
     news['title'] = title
     news['link'] = link
     news['date'] = date
-    all_news.append(news)
-pprint(all_news)
-print('test')
+    # all_news.append(news)
+    try:
+        news_db.update_one({'link': news['link']}, {'$set': news}, upsert=True)
+    except Exception:
+        pprint(f'Ошибка загрузки')
 
-#     news = {}
-#     title = item.xpath(".//h3[@class='s-item__title']/text()")
-#     link = item.xpath(".//h3[@class='s-item__title']/../@href")
-#     price = item.xpath(".//span[@class='s-item__price']//text()")
-#     info = item.xpath(".//span[contains(@class,'s-item__hotness')]/span/text()")
-#
-#     phone['name'] = name
-#     phone['link'] = link
-#     phone['price'] = price
-#     phone['info'] = info
-#     all_phones.append(phone)
-#
-# pprint(all_phones)
-#
-# pprint(info)
+# pprint(all_news)
